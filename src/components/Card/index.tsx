308dac3f49e67d7e04984f9e17f30ad39c725b17
Houssem eddine Andolssi <andolsihoussemeddine@gmail.com>
2024-05-18 01:39:21 +0100

    fix: minor fix

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -28,12 +28,14 @@ const Card = ({
     <div
       className={`p-1 min-[400px]:p-4 rounded-md my-4 flex justify-between ${
         index % 2 === 0 ? 'sm:flex-row-reverse' : 'sm:flex-row'
-      } flex-col hover:scale-105 hover:cursor-pointer dark:text-stone-200 transition-all ease-in-out`}
-      onClick={() => {
-        window.open(link, '_blank', 'noopener,noreferrer');
-      }}
+      } flex-col`}
     >
-      <div className="relative w-full p-3 sm:p-0">
+      <div
+        className="relative w-full p-3 sm:p-0 hover:scale-105 hover:cursor-pointer dark:text-stone-200 transition-all ease-in-out"
+        onClick={() => {
+          window.open(link, '_blank', 'noopener,noreferrer');
+        }}
+      >
         <Image
           className="relative dark:drop-shadow-[0_0_0.2rem_#ffffff70] object-fill filter drop-shadow-lg max-w-full h-auto w-full rounded-md"
           src={`${
@@ -50,7 +52,12 @@ const Card = ({
       </div>
       <div className="flex flex-col justify-center h-full px-8 mt-3 sm:m-0  text-center sm:text-start">
         <div>
-          <h1 className="text-2xl font-semibold">{headLine}</h1>
+          <h1 className="text-2xl font-semibold hover:text-[--primary-color] hover:-translate-y-[3px] transition-all ease-in-out">
+            {' '}
+            <Link target="_blank" rel="noopener noreferrer" href={link}>
+              {headLine}
+            </Link>
+          </h1>
           <h3 className="text-sm sm:text-base font-semibold">{subHeadLine}</h3>
         </div>
         <div>
@@ -68,8 +75,12 @@ const Card = ({
         </div>
         <div className="flex flex-row mt-5 justify-center">
           <Link
-            className={`w-full ${'text-[--primary-color] dark:hover:text-slate-200'}  rounded-md py-3 filter drop-shadow-lg 
-             font-semibold text-lg`}
+            className={`w-full ${
+              index % 2 === 0
+                ? 'text-[--primary-color] hover:text-slate-900 dark:hover:text-slate-200'
+                : 'hover:text-[--primary-color]'
+            } rounded-md py-3 filter drop-shadow-lg 
+            hover:translate-y-1 hover:scale-105 transition-all font-semibold text-lg ease-in-out`}
             target="_blank"
             rel="noopener noreferrer"
             href={link}
